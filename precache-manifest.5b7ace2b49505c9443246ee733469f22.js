40264c410e1e179b533344e46534c2cce5c196eb
EmmanuelBMS <acquabriall2020@gmail.com>
2022-11-16 19:31:02 -0300

    Updates

---
+++ precache-manifest.5b7ace2b49505c9443246ee733469f22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92053f32385e58eacb41e431db8d6179",
+    "url": "/trivia-react-redux/index.html"
+  },
+  {
+    "revision": "5b5642fe14e3e9e7c35f",
+    "url": "/trivia-react-redux/static/css/main.bb87e6f0.chunk.css"
+  },
+  {
+    "revision": "9de766fc6bc667e05ab8",
+    "url": "/trivia-react-redux/static/js/2.f7b7dda5.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/trivia-react-redux/static/js/2.f7b7dda5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5b5642fe14e3e9e7c35f",
+    "url": "/trivia-react-redux/static/js/main.3c1126f2.chunk.js"
+  },
+  {
+    "revision": "c9265d93de189acb100f",
+    "url": "/trivia-react-redux/static/js/runtime-main.460e2238.js"
+  }
+]);
